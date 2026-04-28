7a8586c64ff72be17b6d94e191ec66b58728f525
zlata <yilmazmehmet7711@gmail.com>
2026-04-28 15:14:17 +0300

    refarns tanimlanmasi ve context sinifi olusturulmasi

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,29 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DIDIM\\SQLEXPRESS;database=CoreBlogDb;integrated security=true");
+        }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<MenuItem> MenuItems { get; set; }
+        public DbSet<Restaurant> Restaurants { get; set; }
+
+        public DbSet<Theme> Themes { get; set; }
+        public DbSet<User> Users { get; set; }
+
+
+        }
+}
--- EntityLayer/Concrete/User.cs
+++ EntityLayer/Concrete/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace EntityLayer.Concrete
 {
     public class User
     {
+        [Key]
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
